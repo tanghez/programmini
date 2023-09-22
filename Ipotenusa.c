6e2a41224dfbbc11963b06b4a5eebc37ed16c55b
tanghez <145751204+tanghez@users.noreply.github.com>
2023-09-22 10:06:22 +0200

    Update Ipotenusa.c

--- Ipotenusa.c
+++ Ipotenusa.c
@@ -1,8 +1,23 @@
+//includere libreria generale per printf e scanf
 #include <stdio.h>
+//inludere libreria radice quadrata
+#include <math.h>
 int main ()
 {
-  int cateto1, cateto2;
+  //dichiarare variabili locali
+  int cateto1 = 0, cateto2 = 0;
+  double ipotenusa = 0.0;
+  //chiedere al cateto il primo cateto
   printf("inserire primo cateto: \n");
-  scanf("%d", cateto1);
+  //acquisire il primo cateto
+  scanf("%d", &cateto1);
+  //printf("il cateto inserito è: %d\n", cateto1);
   printf("inserire secondo cateto: \n");
+  //acquisire il secondo cateto
+  scanf("%d", &cateto2);
+  //calcolare iootenusa
+  int somma_quadrati = (cateto1*cateto1)+(cateto2*cateto2);
+  ipotenusa = sqrt(somma_quadrati);
+  //stampare ipotenusa
+  printf("l'ipotenusa vale: %lf\n", ipotenusa);
 }
